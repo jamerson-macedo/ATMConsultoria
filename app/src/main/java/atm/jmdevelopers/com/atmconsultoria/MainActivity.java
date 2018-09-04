6318bb99a2af5cb72b6fffec2db7d860dea607d5
jamerson_macedo@hotmail.com <jamerson_macedo@hotmail.com>
2018-09-03 22:37:49 -0300

    adicionando novas funções

--- app/src/main/java/atm/jmdevelopers/com/atmconsultoria/MainActivity.java
+++ app/src/main/java/atm/jmdevelopers/com/atmconsultoria/MainActivity.java
@@ -1,10 +1,11 @@
 package atm.jmdevelopers.com.atmconsultoria;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +13,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import atm.jmdevelopers.com.atmconsultoria.fragments.ClientesFragment;
+import atm.jmdevelopers.com.atmconsultoria.fragments.Principal_Fragment;
+import atm.jmdevelopers.com.atmconsultoria.fragments.ServicoFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -22,13 +28,17 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // chama a principal
 
+        Principal_Fragment principal_fragment=new Principal_Fragment();
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.framecontainer,principal_fragment);
+        fragmentTransaction.commit();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                enviaremail();
             }
         });
 
@@ -81,12 +91,27 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_priincipal) {
+            Principal_Fragment principal_fragment=new Principal_Fragment();
+            FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.framecontainer,principal_fragment);
+            fragmentTransaction.commit();
+
+
             // Handle the camera action
         } else if (id == R.id.nav_servico) {
+            ServicoFragment servicoFragment=new ServicoFragment();
+            FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.framecontainer,servicoFragment);
+            fragmentTransaction.commit();
 
         } else if (id == R.id.nav_clientes) {
+            ClientesFragment clientesFragment=new ClientesFragment();
+            FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.framecontainer,clientesFragment);
+            fragmentTransaction.commit();
 
         } else if (id == R.id.nav_contato) {
+            enviaremail();
 
         } else if (id == R.id.nav_sobre) {
 
@@ -95,5 +120,17 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    public void enviaremail(){
+        Intent email=new Intent(Intent.ACTION_SEND);
+        email.putExtra(Intent.EXTRA_EMAIL,new String[]{"jamerson_macedo@hotmail.com"});
+        email.putExtra(Intent.EXTRA_SUBJECT,"Contato pelo app");
+        email.putExtra(Intent.EXTRA_TEXT,"Menssagem automatica");
+        // configurar app para abrir
+        email.setType("message/rfc822");
+        startActivity(Intent.createChooser(email,"escolha o app para abrir"));
+
+
+
     }
 }
---
+++ app/src/main/java/atm/jmdevelopers/com/atmconsultoria/fragments/ServicoFragment.java
@@ -0,0 +1,30 @@
+package atm.jmdevelopers.com.atmconsultoria.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import atm.jmdevelopers.com.atmconsultoria.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ServicoFragment extends Fragment {
+
+
+    public ServicoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_servico, container, false);
+    }
+
+}
